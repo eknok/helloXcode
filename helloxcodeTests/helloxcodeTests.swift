4a67aa74fe444e8201584e810aeaf565da0cd046
EikiKanaoka <kakakaeee@gmail.com>
2018-07-03 19:25:38 +0900

    sample Xcode

---
+++ helloxcodeTests/helloxcodeTests.swift
@@ -0,0 +1,36 @@
+//
+//  helloxcodeTests.swift
+//  helloxcodeTests
+//
+//  Created by 金岡栄輝 on 2018/06/29.
+//  Copyright © 2018年 Eiki. All rights reserved.
+//
+
+import XCTest
+@testable import helloxcode
+
+class helloxcodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
